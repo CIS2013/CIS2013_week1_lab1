783d544c89e17cba83398f7b56bdfa5bb29fb380
Natasha S Love <debora@inbox.ru>
2018-01-17 20:34:24 -0600

    first lesson c++

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+string something;
+cout << "Type a character and hit enter";
+cin >> something;
+cout << "You typed" << something;
+return 0;
+}
